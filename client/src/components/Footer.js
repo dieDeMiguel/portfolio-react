f1247a0e0461c59eadb94a59a3f047191836c7ba
dieDeMiguel <diedemiguel@gmail.com>
2021-05-08 10:42:31 +0200

    open/close header on mobile viewports implemented

--- client/src/components/Footer.js
+++ client/src/components/Footer.js
@@ -1,33 +1,33 @@
 function Footer() {
     return (
-        <footer class="footer bg-white relative pt-1 pb-6  border-blue-700">
-            <div class="container mx-auto px-6">
-                <div class="sm:flex sm:mt-14 sm:z-50">
-                    <div class="mt-8 sm:mt-0 sm:w-full sm:px-8 flex flex-col md:flex-row justify-between">
-                        <div class="flex flex-col">
-                            <span class="font-bold text-gray-700 uppercase mb-2">
+        <footer className="footer bg-white relative pt-1 pb-6  border-blue-700">
+            <div className="container mx-auto px-6">
+                <div className="sm:flex sm:mt-14 sm:z-50">
+                    <div className="mt-8 sm:mt-0 sm:w-full sm:px-8 flex flex-col md:flex-row justify-between">
+                        <div className="flex flex-col">
+                            <span className="font-bold text-gray-700 uppercase mb-2">
                                 Contact
                             </span>
-                            <span class="my-2">
+                            <span className="my-2">
                                 <a
                                     href="/about"
-                                    class="text-blue-700  text-md hover:text-blue-500"
+                                    className="text-blue-700  text-md hover:text-blue-500"
                                 >
                                     About
                                 </a>
                             </span>
-                            <span class="my-2">
+                            <span className="my-2">
                                 <a
                                     href="mailto:diedemiguel@gmail.com"
-                                    class="text-blue-700  text-md hover:text-blue-500"
+                                    className="text-blue-700  text-md hover:text-blue-500"
                                 >
                                     E-Mail
                                 </a>
                             </span>
-                            <span class="my-2">
+                            <span className="my-2">
                                 <a
                                     href="https://www.linkedin.com/in/diedemiguel/"
-                                    class="text-blue-700  text-md hover:text-blue-500"
+                                    className="text-blue-700  text-md hover:text-blue-500"
                                 >
                                     LinkedIn
                                 </a>
@@ -36,10 +36,10 @@ function Footer() {
                     </div>
                 </div>
             </div>
-            <div class="container mx-auto px-6">
-                <div class="mt-16 border-t-2 border-gray-300 flex flex-col items-center">
-                    <div class="sm:w-2/3 text-center pt-3">
-                        <p class="text-sm text-blue-700 font-bold mb-2">
+            <div className="container mx-auto px-6">
+                <div className="mt-16 border-t-2 border-gray-300 flex flex-col items-center">
+                    <div className="sm:w-2/3 text-center pt-3">
+                        <p className="text-sm text-blue-700 font-bold mb-2">
                             © 2021 Diego de Miguel
                         </p>
                     </div>
--- client/src/components/HomePage.js
+++ client/src/components/HomePage.js
@@ -1,16 +1,23 @@
+import { useState } from "react";
 import Footer from "./Footer";
 
 function HomePage() {
+    const [showMenu, setShowMenu] = useState(false);
+
+    function onMenuClick() {
+        setShowMenu(!showMenu);
+    }
+
     return (
         <>
-            <div class="relative bg-white overflow-hidden" id="main-banner">
-                <div class="max-w-7xl mx-auto">
+            <div className="relative bg-white overflow-hidden" id="main-banner">
+                <div className="max-w-7xl mx-auto">
                     <div
-                        class="relative z-10 pb-8 bg-white sm:pb-16 md:pb-20 lg:max-w-2xl lg:w-full lg:pb-28 xl:pb-32"
+                        className="relative z-10 pb-8 bg-white sm:pb-16 md:pb-20 lg:max-w-2xl lg:w-full lg:pb-28 xl:pb-32"
                         id="main-banner"
                     >
                         <svg
-                            class="hidden lg:block absolute right-0 inset-y-0 h-full w-48 text-white transform translate-x-1/2"
+                            className="hidden lg:block absolute right-0 inset-y-0 h-full w-48 text-white transform translate-x-1/2"
                             fill="currentColor"
                             viewBox="0 0 100 100"
                             preserveAspectRatio="none"
@@ -19,32 +26,33 @@ function HomePage() {
                             <polygon points="50,0 100,0 50,100 0,100" />
                         </svg>
 
-                        <div class="relative pt-6 px-4 sm:px-6 lg:px-8">
+                        <div className="relative pt-6 px-4 sm:px-6 lg:px-8">
                             <nav
-                                class="relative flex items-center justify-between sm:h-10 lg:justify-start"
+                                className="relative flex items-center justify-between sm:h-10 lg:justify-start"
                                 aria-label="Global"
                             >
-                                <div class="flex items-center flex-grow flex-shrink-0 lg:flex-grow-0">
-                                    <div class="flex items-center justify-between w-full md:w-auto">
+                                <div className="flex items-center flex-grow flex-shrink-0 lg:flex-grow-0">
+                                    <div className="flex items-center justify-between w-full md:w-auto">
                                         <a href="/">
                                             <img
-                                                class="h-8 w-auto sm:h-10"
+                                                className="h-8 w-auto sm:h-10"
                                                 src="/layer-group-solid.svg"
                                             />
                                         </a>
-                                        <div class="-mr-2 flex items-center md:hidden">
+                                        <div className="-mr-2 flex items-center md:hidden">
                                             <button
+                                                onClick={onMenuClick}
                                                 id="hamburger"
                                                 type="button"
-                                                class="bg-white rounded-md p-2 inline-flex items-center justify-center text-gray-400 hover:text-gray-500 hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-inset focus:ring-indigo-500"
+                                                className="bg-white rounded-md p-2 inline-flex items-center justify-center text-gray-400 hover:text-gray-500 hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-inset focus:ring-indigo-500"
                                                 aria-expanded="false"
                                             >
-                                                <span class="sr-only">
+                                                <span className="sr-only">
                                                     Open main menu
                                                 </span>
 
                                                 <svg
-                                                    class="h-6 w-6"
+                                                    className="h-6 w-6"
                                                     xmlns="http://www.w3.org/2000/svg"
                                                     fill="none"
                                                     viewBox="0 0 24 24"
@@ -52,9 +60,9 @@ function HomePage() {
                                                     aria-hidden="true"
                                                 >
                                                     <path
-                                                        stroke-linecap="round"
-                                                        stroke-linejoin="round"
-                                                        stroke-width="2"
+                                                        strokeLinecap="round"
+                                                        strokeLinejoin="round"
+                                                        strokeWidth="2"
                                                         d="M4 6h16M4 12h16M4 18h16"
                                                     />
                                                 </svg>
@@ -62,31 +70,31 @@ function HomePage() {
                                         </div>
                                     </div>
                                 </div>
-                                <div class="hidden md:block md:ml-10 md:pr-4 md:space-x-8">
+                                <div className="hidden md:block md:ml-10 md:pr-4 md:space-x-8">
                                     <a
                                         href="/"
-                                        class="font-medium text-gray-500 hover:text-gray-900"
+                                        className="font-medium text-gray-500 hover:text-gray-900"
                                     >
                                         Home
                                     </a>
 
                                     <a
                                         href="/projects"
-                                        class="font-medium text-gray-500 hover:text-gray-900"
+                                        className="font-medium text-gray-500 hover:text-gray-900"
                                     >
                                         Projects
                                     </a>
 
                                     <a
                                         href="/contact"
-                                        class="font-medium text-gray-500 hover:text-gray-900"
+                                        className="font-medium text-gray-500 hover:text-gray-900"
                                     >
                                         Contact
                                     </a>
 
                                     <a
                                         href="/about"
-                                        class="font-medium text-gray-500 hover:text-gray-900"
+                                        className="font-medium text-gray-500 hover:text-gray-900"
                                     >
                                         About
                                     </a>
@@ -94,27 +102,35 @@ function HomePage() {
                             </nav>
                         </div>
 
-                        <div class="absolute top-0 inset-x-0 p-2 transition transform origin-top-right hidden header-menu">
-                            <div class="rounded-lg shadow-md bg-white ring-1 ring-black ring-opacity-5 overflow-hidden">
-                                <div class="px-5 pt-4 flex items-center justify-between">
+                        <div
+                            className={
+                                showMenu
+                                    ? "absolute top-0 inset-x-0 p-2 transition transform origin-top-right header-menu"
+                                    : "absolute top-0 inset-x-0 p-2 transition transform origin-top-right hidden header-menu"
+                            }
+                        >
+                            <div className="rounded-lg shadow-md bg-white ring-1 ring-black ring-opacity-5 overflow-hidden">
+                                <div className="px-5 pt-4 flex items-center justify-between">
                                     <div>
                                         <img
-                                            class="h-8 w-auto"
+                                            className="h-8 w-auto"
                                             src="/layer-group-solid.svg"
                                             alt=""
                                         />
                                     </div>
-                                    <div class="-mr-2">
+                                    <div className="-mr-2">
                                         <button
+                                            onClick={onMenuClick}
                                             type="button"
-                                            class="bg-white rounded-md p-2 inline-flex items-center justify-center text-gray-400 hover:text-gray-500 hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-inset focus:ring-indigo-500"
+                                            className="bg-white rounded-md p-2 inline-flex items-center justify-center text-gray-400 hover:text-gray-500 hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-inset focus:ring-indigo-500"
                                         >
-                                            <span class="sr-only">
+                                            <span className="sr-only">
                                                 Close main menu
                                             </span>
 
                                             <svg
-                                                class="h-6 w-6 close-header"
+                                                onClick={onMenuClick}
+                                                className="h-6 w-6 close-header"
                                                 xmlns="http://www.w3.org/2000/svg"
                                                 fill="none"
                                                 viewBox="0 0 24 24"
@@ -122,40 +138,40 @@ function HomePage() {
                                                 aria-hidden="true"
                                             >
                                                 <path
-                                                    stroke-linecap="round"
-                                                    stroke-linejoin="round"
-                                                    stroke-width="2"
+                                                    strokeLinecap="round"
+                                                    strokeLinejoin="round"
+                                                    strokeWidth="2"
                                                     d="M6 18L18 6M6 6l12 12"
                                                 />
                                             </svg>
                                         </button>
                                     </div>
                                 </div>
-                                <div class="px-2 pt-2 pb-3 space-y-1">
+                                <div className="px-2 pt-2 pb-3 space-y-1">
                                     <a
                                         href="/"
-                                        class="block px-3 py-2 rounded-md text-base font-medium text-gray-700 hover:text-gray-900 hover:bg-gray-50"
+                                        className="block px-3 py-2 rounded-md text-base font-medium text-gray-700 hover:text-gray-900 hover:bg-gray-50"
                                     >
                                         Home
                                     </a>
 
                                     <a
                                         href="/projects"
-                                        class="block px-3 py-2 rounded-md text-base font-medium text-gray-700 hover:text-gray-900 hover:bg-gray-50"
+                                        className="block px-3 py-2 rounded-md text-base font-medium text-gray-700 hover:text-gray-900 hover:bg-gray-50"
                                     >
                                         Projects
                                     </a>
 
                                     <a
                                         href="/contact"
-                                        class="block px-3 py-2 rounded-md text-base font-medium text-gray-700 hover:text-gray-900 hover:bg-gray-50"
+                                        className="block px-3 py-2 rounded-md text-base font-medium text-gray-700 hover:text-gray-900 hover:bg-gray-50"
                                     >
                                         Contact
                                     </a>
 
                                     <a
                                         href="/Blog"
-                                        class="block px-3 py-2 rounded-md text-base font-medium text-gray-700 hover:text-gray-900 hover:bg-gray-50"
+                                        className="block px-3 py-2 rounded-md text-base font-medium text-gray-700 hover:text-gray-900 hover:bg-gray-50"
                                     >
                                         Blog
                                     </a>
@@ -163,25 +179,25 @@ function HomePage() {
                             </div>
                         </div>
 
-                        <main class="mt-10 mx-auto max-w-7xl px-4 sm:mt-12 sm:px-6 md:mt-16 lg:mt-20 lg:px-8 xl:mt-28 main-title">
-                            <div class="sm:text-center lg:text-left">
-                                <h1 class="text-4xl tracking-tight font-extrabold text-gray-900 sm:text-5xl md:text-6xl">
-                                    <span class="block xl:inline">
+                        <main className="mt-10 mx-auto max-w-7xl px-4 sm:mt-12 sm:px-6 md:mt-16 lg:mt-20 lg:px-8 xl:mt-28 main-title">
+                            <div className="sm:text-center lg:text-left">
+                                <h1 className="text-4xl tracking-tight font-extrabold text-gray-900 sm:text-5xl md:text-6xl">
+                                    <span className="block xl:inline">
                                         Diego de Miguel
                                     </span>
-                                    <span class="block text-indigo-600 xl:inline">
+                                    <span className="block text-indigo-600 xl:inline">
                                         Full-Stack Software Developer
                                     </span>
                                 </h1>
-                                <p class="mt-3 text-base text-gray-500 sm:mt-5 sm:text-lg sm:max-w-xl sm:mx-auto md:mt-5 md:text-xl lg:mx-0">
+                                <p className="mt-3 text-base text-gray-500 sm:mt-5 sm:text-lg sm:max-w-xl sm:mx-auto md:mt-5 md:text-xl lg:mx-0">
                                     A comprehensive overview of my projects and
                                     interests.
                                 </p>
-                                <div class="mt-5 sm:mt-8 sm:flex sm:justify-center lg:justify-start">
-                                    <div class="rounded-md shadow">
+                                <div className="mt-5 sm:mt-8 sm:flex sm:justify-center lg:justify-start">
+                                    <div className="rounded-md shadow">
                                         <a
                                             href="/projects"
-                                            class="w-full flex items-center justify-center px-8 py-3 border border-transparent text-base font-medium rounded-md text-white bg-indigo-600 hover:bg-indigo-700 md:py-4 md:text-lg md:px-10"
+                                            className="w-full flex items-center justify-center px-8 py-3 border border-transparent text-base font-medium rounded-md text-white bg-indigo-600 hover:bg-indigo-700 md:py-4 md:text-lg md:px-10"
                                         >
                                             Projects
                                         </a>
@@ -192,9 +208,9 @@ function HomePage() {
                     </div>
                 </div>
 
-                <div class="lg:absolute lg:inset-y-0 lg:right-0 lg:w-1/2">
+                <div className="lg:absolute lg:inset-y-0 lg:right-0 lg:w-1/2">
                     <img
-                        class="h-56 w-full object-cover sm:h-72 md:h-96 lg:w-full lg:h-full"
+                        className="h-56 w-full object-cover sm:h-72 md:h-96 lg:w-full lg:h-full"
                         src="./jump.jpeg"
                         alt=""
                     />
