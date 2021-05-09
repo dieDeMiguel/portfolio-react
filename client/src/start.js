91742cd221fab42e39d6c7eee24ed0b2d361d9cd
dieDeMiguel <diedemiguel@gmail.com>
2021-05-09 11:10:09 +0200

    refactored start.js

--- client/src/start.js
+++ client/src/start.js
@@ -1,12 +1,9 @@
 import ReactDOM from "react-dom";
 import Homepage from "./components/HomePage";
-import Contact from "./components/Contact";
 import App from "./components/App";
 
 if (window.location.pathname === "/") {
     ReactDOM.render(<Homepage />, document.querySelector("main"));
-} else if (window.location.pathname === "/contact") {
-    ReactDOM.render(<Contact />, document.querySelector("main"));
 } else {
     ReactDOM.render(<App />, document.querySelector("main"));
 }
