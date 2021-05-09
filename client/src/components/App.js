45844deb558a6762cf2ac67b68e0f504710c539a
dieDeMiguel <diedemiguel@gmail.com>
2021-05-09 11:09:55 +0200

    created several routes inside the App component

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,17 +1,31 @@
 import { BrowserRouter, Route, Link } from "react-router-dom";
 import HomePage from "./HomePage";
 import Contact from "./Contact";
+import Header from "./Header";
+import Footer from "./Footer";
+import About from "./About";
+import Projects from "./Projects";
 
 function App() {
     return (
-        <BrowserRouter>
-            <Route path="/" exact>
-                <HomePage />
-            </Route>
-            <Route path="/contact">
-                <Contact />
-            </Route>
-        </BrowserRouter>
+        <>
+            <Header />
+            <BrowserRouter>
+                <Route path="/" exact>
+                    <HomePage />
+                </Route>
+                <Route path="/contact">
+                    <Contact />
+                </Route>
+                <Route path="/about">
+                    <About />
+                </Route>
+                <Route path="/projects">
+                    <Projects />
+                </Route>
+            </BrowserRouter>
+            <Footer />
+        </>
     );
 }
 
