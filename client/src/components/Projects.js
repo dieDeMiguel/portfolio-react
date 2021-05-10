01400b4be997bc717374d9732ae8e98134b5eda4
dieDeMiguel <diedemiguel@gmail.com>
2021-05-10 11:33:55 +0200

    projects component refactored

--- client/src/components/Projects.js
+++ client/src/components/Projects.js
@@ -1,5 +1,3 @@
-import { useEffect } from "react";
-
 function Projects({ projects }) {
     return (
         <>
@@ -12,31 +10,32 @@ function Projects({ projects }) {
             </div>
             <div className="flex items-center justify-center mt-32 mb-40  0">
                 <div className="grid grid-cols-1 gap-6 sm:grid-cols-2 md:grid-cols-3 xl:grid-cols-4 xl:grid-cols-4">
-                    {projects.map((project) => (
-                        <div
-                            key={project.slug}
-                            className="relative bg-white pb-5  rounded-3xl w-72 sm:w-60 2xl:w-72 my-2 shadow-xl"
-                        >
-                            <img
-                                className="w-full h-2/4 object-cover"
-                                src={project.img}
-                                alt={project.title}
-                            />
-                            <div className="mt-8 px-5 md:px-3 lg:px-5">
-                                <p className="text-xl font-semibold my-4">
-                                    <a href="/project/{{slug}}">
-                                        {project.heading}
-                                    </a>
-                                </p>
-                                <div className="flex space-x-2 text-gray-400 text-sm my-3">
-                                    {project.date}
-                                </div>
-                                <div className="flex space-x-2 text-gray-600 text-sm">
-                                    <p>{project.subtitle}</p>
+                    {projects.map((project) => {
+                        const href = `/project/${project.slug}`;
+                        return (
+                            <div
+                                key={project.slug}
+                                className="relative bg-white pb-5  rounded-3xl w-72 sm:w-60 2xl:w-72 my-2 shadow-xl"
+                            >
+                                <img
+                                    className="w-full h-2/4 object-cover"
+                                    src={project.img}
+                                    alt={project.title}
+                                />
+                                <div className="mt-8 px-5 md:px-3 lg:px-5">
+                                    <p className="text-xl font-semibold my-4">
+                                        <a href={href}>{project.heading}</a>
+                                    </p>
+                                    <div className="flex space-x-2 text-gray-400 text-sm my-3">
+                                        {project.date}
+                                    </div>
+                                    <div className="flex space-x-2 text-gray-600 text-sm">
+                                        <p>{project.subtitle}</p>
+                                    </div>
                                 </div>
                             </div>
-                        </div>
-                    ))}
+                        );
+                    })}
                 </div>
             </div>
         </>
